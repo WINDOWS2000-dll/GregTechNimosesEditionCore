bb87d03d5fc4ec1e05c41f4adf4244f135e49652
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-03-07 20:05:43 +0900

    テクスチャ関連の変更、修正

--- src/main/java/gtne/api/gui/GTNEGuiTextures.java
+++ src/main/java/gtne/api/gui/GTNEGuiTextures.java
@@ -4,5 +4,5 @@ import gregtech.api.gui.resources.TextureArea;
 
 public class GTNEGuiTextures {
     //GTNECore
-    public static final TextureArea PROGRESSBAR_ADVANDED_PRECISION_ASSEMBLY_LINE = TextureArea.fullImage("progress_bar_advanced_precision_assembly_line.png");
+    public static final TextureArea PROGRESSBAR_ADVANDED_PRECISION_ASSEMBLY_LINE = TextureArea.fullImage("textures/gui/progress_bar_advanced_precision_assembly_line.png");
 }
