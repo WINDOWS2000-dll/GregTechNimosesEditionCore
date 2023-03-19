ebb7630a761068d55869d691bee80b459b2c0ec0
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-03-20 02:23:51 +0900

    レシピテスト

---
+++ src/main/java/gtne/loaders/recipes/SingleBlocks/ComponentsAssembler.java
@@ -0,0 +1,31 @@
+package gtne.loaders.recipes.SingleBlocks;
+
+import gregtech.api.recipes.ModHandler;
+import gregtech.api.unification.ore.OrePrefix;
+import gtne.api.recipes.GTNERecipeMaps;
+
+import javax.annotation.Nonnull;
+
+import static gregtech.api.unification.ore.OrePrefix.*;
+import static gregtech.common.items.MetaItems.*;
+import static gregtech.loaders.recipe.MetaTileEntityLoader.registerMachineRecipe;
+import static gregtech.loaders.recipe.CraftingComponent.*;
+import static gregtech.api.GTValues.*;
+import static gregtech.api.unification.material.Materials.*;
+public class ComponentsAssembler {
+
+    public static void init() {
+
+        //MV Components
+        ModHandler.removeRecipeByName("gregtech:electric_motor_mv");
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(stick, Aluminium, 2)
+                .input(stick, SteelMagnetic, 1)
+                .input(wireGtDouble, Cupronickel, 4)
+                .input(cableGtSingle, Copper, 2)
+                .fluidInputs(Lubricant.getFluid(144))
+                .fluidInputs(SolderingAlloy.getFluid(72))
+                .duration(120).EUt(VA[LV])
+                .buildAndRegister();
+    }
+}
