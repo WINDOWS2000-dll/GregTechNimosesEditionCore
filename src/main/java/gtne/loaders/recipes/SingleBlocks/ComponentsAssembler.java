529eb61d12b6b49d947039062b3439b5862b533a
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-03-23 18:07:49 +0900

    EV Componentsレシピ追加

--- src/main/java/gtne/loaders/recipes/SingleBlocks/ComponentsAssembler.java
+++ src/main/java/gtne/loaders/recipes/SingleBlocks/ComponentsAssembler.java
@@ -258,7 +258,7 @@ public class ComponentsAssembler {
 
         GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
                 .input(stick, SteelMagnetic, 1)
-                .input(stick, Silver, 2)
+                .input(stick, StainlessSteel, 2)
                 .input(wireGtDouble, Electrum, 4)
                 .input(cableGtDouble, Silver, 2)
                 .fluidInputs(Lubricant.getFluid(160))
@@ -457,6 +457,216 @@ public class ComponentsAssembler {
                 .duration(other_duration).EUt(VA[MV])
                 .buildAndRegister();
 
+
+        //EV Components
+        //Motor
+        ModHandler.removeRecipeByName("gregtech:electric_motor_ev");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtDouble, Aluminium, 2), OreDictUnifier.get(stick, Titanium,  2),
+                                                                            OreDictUnifier.get(stick, NeodymiumMagnetic, 1), OreDictUnifier.get(wireGtDouble, Kanthal, 4));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtDouble, Aluminium, 2)
+                .input(stick, Titanium, 2)
+                .input(stick, NeodymiumMagnetic, 1)
+                .input(cableGtDouble, Kanthal, 4)
+                .input(wireFine, Copper, 64)
+                .fluidInputs(Lubricant.getFluid(160))
+                .fluidInputs(SolderingAlloy.getFluid(halfsold))
+                .output(ELECTRIC_MOTOR_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+
+        //Pump
+        ModHandler.removeRecipeByName("gregtech:electric_pump_ev_rubber");
+        ModHandler.removeRecipeByName("gregtech:electric_pump_ev_silicone_rubber");
+        ModHandler.removeRecipeByName("gregtech:electric_pump_ev_styrene_butadiene_rubber");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtSingle, Aluminium, 1), OreDictUnifier.get(pipeNormalFluid, Titanium, 1), OreDictUnifier.get(screw, StainlessSteel, 1), OreDictUnifier.get(rotor, StainlessSteel, 1), OreDictUnifier.get(ring, Rubber, 2), ELECTRIC_MOTOR_EV.getStackForm(1));
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtSingle, Aluminium, 1), OreDictUnifier.get(pipeNormalFluid, Titanium, 1), OreDictUnifier.get(screw, StainlessSteel, 1), OreDictUnifier.get(rotor, StainlessSteel, 1), OreDictUnifier.get(ring, SiliconeRubber, 2), ELECTRIC_MOTOR_EV.getStackForm(1));
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtSingle, Aluminium, 1), OreDictUnifier.get(pipeNormalFluid, Titanium, 1), OreDictUnifier.get(screw, StainlessSteel, 1), OreDictUnifier.get(rotor, StainlessSteel, 1), OreDictUnifier.get(ring, StyreneButadieneRubber, 2), ELECTRIC_MOTOR_EV.getStackForm(1));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtSingle, Aluminium, 2)
+                .input(pipeNormalFluid, Titanium, 1)
+                .input(screw, StainlessSteel, 2)
+                .input(rotor, StainlessSteel, 1)
+                .input(ring, Rubber, 4)
+                .input(ELECTRIC_MOTOR_EV, 1)
+                .fluidInputs(Lubricant.getFluid(180))
+                .fluidInputs(SolderingAlloy.getFluid(sold))
+                .output(ELECTRIC_PUMP_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtSingle, Aluminium, 2)
+                .input(pipeNormalFluid, Titanium, 1)
+                .input(screw, StainlessSteel, 2)
+                .input(rotor, StainlessSteel, 1)
+                .input(ring, SiliconeRubber, 2)
+                .input(ELECTRIC_MOTOR_EV, 1)
+                .fluidInputs(Lubricant.getFluid(180))
+                .fluidInputs(SolderingAlloy.getFluid(sold))
+                .output(ELECTRIC_PUMP_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtSingle, Aluminium, 2)
+                .input(pipeNormalFluid, Titanium, 1)
+                .input(screw, StainlessSteel, 2)
+                .input(rotor, StainlessSteel, 1)
+                .input(ring, StyreneButadieneRubber, 2)
+                .input(ELECTRIC_MOTOR_EV, 1)
+                .fluidInputs(Lubricant.getFluid(180))
+                .fluidInputs(SolderingAlloy.getFluid(sold))
+                .output(ELECTRIC_PUMP_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+
+        //Conveyor
+        ModHandler.removeRecipeByName("gregtech:conveyor_module_ev_rubber");
+        ModHandler.removeRecipeByName("gregtech:conveyor_module_ev_silicone_rubber");
+        ModHandler.removeRecipeByName("gregtech:conveyor_module_ev_styrene_butadiene_rubber");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtSingle, Aluminium, 1), OreDictUnifier.get(plate, StyreneButadieneRubber, 6), ELECTRIC_MOTOR_EV.getStackForm(2), IntCircuitIngredient.getIntegratedCircuit(1));
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtSingle, Aluminium, 1), OreDictUnifier.get(plate, SiliconeRubber, 6), ELECTRIC_MOTOR_EV.getStackForm(2), IntCircuitIngredient.getIntegratedCircuit(1));
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtSingle, Aluminium, 1), OreDictUnifier.get(plate, Rubber, 6), ELECTRIC_MOTOR_EV.getStackForm(2), IntCircuitIngredient.getIntegratedCircuit(1));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtSingle, Aluminium, 1)
+                .input(plate, Rubber, 12)
+                .input(ELECTRIC_MOTOR_EV, 2)
+                .input(ring, StainlessSteel, 4)
+                .circuitMeta(1)
+                .fluidInputs(Lubricant.getFluid(160))
+                .fluidInputs(SolderingAlloy.getFluid(halfsold))
+                .output(CONVEYOR_MODULE_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtSingle, Aluminium, 1)
+                .input(plate, SiliconeRubber, 6)
+                .input(ELECTRIC_MOTOR_EV, 2)
+                .input(ring, StainlessSteel, 4)
+                .circuitMeta(1)
+                .fluidInputs(Lubricant.getFluid(160))
+                .fluidInputs(SolderingAlloy.getFluid(halfsold))
+                .output(CONVEYOR_MODULE_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtSingle, Aluminium, 1)
+                .input(plate, StyreneButadieneRubber, 6)
+                .input(ELECTRIC_MOTOR_EV, 2)
+                .input(ring, StainlessSteel, 4)
+                .circuitMeta(1)
+                .fluidInputs(Lubricant.getFluid(160))
+                .fluidInputs(SolderingAlloy.getFluid(halfsold))
+                .output(CONVEYOR_MODULE_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+
+        //Electric Piston
+        ModHandler.removeRecipeByName("gregtech:electric_piston_ev");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(stick, Titanium, 2), OreDictUnifier.get(cableGtSingle, Aluminium, 2),
+                OreDictUnifier.get(plate, Titanium, 3), OreDictUnifier.get(gearSmall, Titanium, 1),
+                ELECTRIC_MOTOR_EV.getStackForm(1));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(stick, Titanium, 3)
+                .input(cableGtSingle, Aluminium, 2)
+                .input(plate, Titanium, 3)
+                .input(gear, Titanium, 1)
+                .input(gearSmall, Titanium, 2)
+                .input(ELECTRIC_MOTOR_EV, 1)
+                .fluidInputs(Lubricant.getFluid(200))
+                .fluidInputs(SolderingAlloy.getFluid(sold))
+                .output(ELECTRIC_PISTON_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+
+        //Robot Arm
+        ModHandler.removeRecipeByName("gregtech:robot_arm_ev");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(cableGtSingle, Aluminium, 3), OreDictUnifier.get(stick, Titanium, 2),
+                ELECTRIC_MOTOR_EV.getStackForm(2), ELECTRIC_PISTON_EV.getStackForm(1),
+                OreDictUnifier.get(circuit, MarkerMaterials.Tier.EV));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(cableGtSingle, Aluminium, 3)
+                .input(stick, Titanium, 4)
+                .input(ELECTRIC_MOTOR_EV, 2)
+                .input(ELECTRIC_PISTON_EV, 1)
+                .input(gearSmall, Titanium, 2)
+                .input(circuit, MarkerMaterials.Tier.EV)
+                .fluidInputs(Lubricant.getFluid(240))
+                .fluidInputs(SolderingAlloy.getFluid(sold * 2))
+                .output(ROBOT_ARM_EV)
+                .duration(robotarm_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+
+        //Field Generator
+        ModHandler.removeRecipeByName("gregtech:field_generator_ev");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(gem, NetherStar, 1), OreDictUnifier.get(plateDouble, Titanium, 2),
+                OreDictUnifier.get(circuit, MarkerMaterials.Tier.EV, 2), OreDictUnifier.get(wireGtQuadruple, UraniumTriplatinum, 4));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(gem, NetherStar, 1)
+                .input(plateDouble, Titanium, 4)
+                .input(circuit, MarkerMaterials.Tier.EV, 4)
+                .input(wireGtQuadruple, UraniumTriplatinum, 4)
+                .input(EMITTER_EV)
+                .input(SENSOR_EV)
+                .fluidInputs(SolderingAlloy.getFluid(sold * 4))
+                .fluidInputs(new FluidStack(TFFluids.fluidEnder, 2500))
+                .fluidInputs(Krypton.getFluid(2000))
+                .output(FIELD_GENERATOR_EV)
+                .duration(field_generator_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+
+        //Emitter
+        ModHandler.removeRecipeByName("gregtech:emitter_ev");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(stick, Platinum, 4), OreDictUnifier.get(cableGtSingle, Aluminium, 2),
+                OreDictUnifier.get(circuit, MarkerMaterials.Tier.EV, 2), QUANTUM_EYE.getStackForm(1),
+                IntCircuitIngredient.getIntegratedCircuit(1));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(stick, Platinum, 6)
+                .input(cableGtSingle, Aluminium, 2)
+                .input(circuit, MarkerMaterials.Tier.EV, 2)
+                .input(QUANTUM_EYE, 1)
+                .input(lens, Diamond, 2)
+                .circuitMeta(1)
+                .fluidInputs(Glass.getFluid(864))
+                .fluidInputs(SolderingAlloy.getFluid(sold * 3))
+                .fluidInputs(Argon.getFluid(1000))
+                .output(EMITTER_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
+
+
+        //Sensor
+        ModHandler.removeRecipeByName("gregtech:sensor_ev");
+        GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES, OreDictUnifier.get(stick, Platinum, 1), OreDictUnifier.get(plate, Titanium, 4),
+                OreDictUnifier.get(circuit, MarkerMaterials.Tier.EV, 1), QUANTUM_EYE.getStackForm(1));
+
+        GTNERecipeMaps.COMPONENTS_ASSEMBLER.recipeBuilder()
+                .input(stick, Platinum, 2)
+                .input(plate, Titanium, 6)
+                .input(cableGtSingle, Aluminium, 2)
+                .input(circuit, MarkerMaterials.Tier.EV, 2)
+                .input(QUANTUM_EYE, 1)
+                .fluidInputs(SolderingAlloy.getFluid(sold * 3))
+                .fluidInputs(new FluidStack(TFFluids.fluidRedstone, 2000))
+                .output(SENSOR_EV)
+                .duration(other_duration).EUt(VA[HV])
+                .buildAndRegister();
 
     }
 }
