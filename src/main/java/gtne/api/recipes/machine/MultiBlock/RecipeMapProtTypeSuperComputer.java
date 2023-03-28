6a1f9f021c777751bb9c67dde3407069f2acbd16
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-03-29 00:02:14 +0900

    試験1

--- src/main/java/gtne/api/recipes/machine/MultiBlock/RecipeMapProtTypeSuperComputer.java
+++ src/main/java/gtne/api/recipes/machine/MultiBlock/RecipeMapProtTypeSuperComputer.java
@@ -23,13 +23,55 @@ public class RecipeMapProtTypeSuperComputer <R extends RecipeBuilder<R>> extends
     @Override
     @Nonnull
     public ModularUI.Builder createJeiUITemplate(IItemHandlerModifiable importItems, IItemHandlerModifiable exportItems, FluidTankList importFluids, FluidTankList exportFluids, int yOffset) {
-        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND, 176, 176)
+        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND, 176, 200)
                 .widget(new ProgressWidget(200, 14, 27, 117, 63, GTNEGuiTextures.PROGRESSBAR_PROTTYPE_SUPERCOMPUTER, ProgressWidget.MoveType.HORIZONTAL));
         this.addInventorySlotGroup(builder, importItems, importFluids, false, yOffset);
         this.addInventorySlotGroup(builder, exportItems, exportFluids, true, yOffset);
         return builder;
     }
 
+    @Override
+    protected void addInventorySlotGroup(ModularUI.Builder builder, @Nonnull IItemHandlerModifiable itemHandler, @Nonnull FluidTankList fluidHandler, boolean isOutputs, int yOffset) {
+        int itemInputsCount = itemHandler.getSlots();
+        int fluidInputsCount = fluidHandler.getTanks();
+        boolean invertFluids = false;
+        if (itemInputsCount == 0) {
+            int tmp = itemInputsCount;
+            itemInputsCount = fluidInputsCount;
+            fluidInputsCount = tmp;
+            invertFluids = true;
+        }
+        int[] inputSlotGrid = determineSlotsGrid(itemInputsCount);
+        int itemSlotsToLeft = inputSlotGrid[0];
+        int itemSlotsToDown = inputSlotGrid[1];
+        int startInputsX = 105 - itemSlotsToLeft * 21;
+        int startInputsY = 45 - (int) (itemSlotsToDown / 2.0 * 21);
+
+        if (!isOutputs) {
+            for (int i = 0; i < itemSlotsToDown; i++) {
+                for (int j = 0; j < itemSlotsToLeft; j++) {
+                    int slotIndex = i * itemSlotsToLeft + j;
+                    addSlot(builder, startInputsX + 18 * j, startInputsY + 18 * i, slotIndex, itemHandler, fluidHandler, invertFluids, false);
+                }
+            }
+            if (fluidInputsCount > 0 || invertFluids) {
+                if (itemSlotsToDown <= fluidInputsCount) {
+                    for (int i = 0; i < fluidInputsCount; i++) {
+                        if (i <= 4) {
+                            addSlot(builder, (startInputsX + 21 * 5) + 3, startInputsY + 18 * i, i, itemHandler, fluidHandler, true, false);
+                        }
+                        if (i >= 5) {
+                            addSlot(builder, (startInputsX + 21 * 8) - 42, (startInputsY + 18 * i) - 90, i, itemHandler, fluidHandler, true, false);
+                        }
+                    }
+                }
+            }
+        } else {
+            addSlot(builder, startInputsX + 18 * 4, 3, 0, itemHandler, fluidHandler, invertFluids, true);
+        }
+    }
+
+    /*
     @Override
     protected void addInventorySlotGroup(ModularUI.Builder builder, @Nonnull IItemHandlerModifiable itemHandler, @Nonnull FluidTankList fluidHandler, boolean isOutputs, int yOffset) {
         int itemInputsCount = itemHandler.getSlots();
@@ -84,4 +126,6 @@ public class RecipeMapProtTypeSuperComputer <R extends RecipeBuilder<R>> extends
             addSlot(builder, 149, 90, 0, itemHandler, fluidHandler, true, true);
         }
     }
+
+     */
 }
