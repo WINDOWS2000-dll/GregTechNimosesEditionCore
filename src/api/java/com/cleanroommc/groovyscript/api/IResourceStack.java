d93fa7ae61d2fd7a6bc56c415d5ec0b6c18fbbb2
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-04-22 03:33:41 +0900

    ビルドスクリプト更新

---
+++ src/api/java/com/cleanroommc/groovyscript/api/IResourceStack.java
@@ -0,0 +1,28 @@
+package com.cleanroommc.groovyscript.api;
+
+
+//import com.sun.istack.internal.NotNull;
+
+/**
+ * An object that has an amount.
+ * Can use '*' in groovy to set amount.
+ */
+public interface IResourceStack {
+
+    int getAmount();
+
+    void setAmount(int amount);
+
+    default
+    IResourceStack withAmount(int amount) {
+        setAmount(amount);
+        return this;
+    }
+
+    /**
+     * enables groovy to use '*' operator
+     */
+    default IResourceStack multiply(Number n) {
+        return withAmount(n.intValue());
+    }
+}
