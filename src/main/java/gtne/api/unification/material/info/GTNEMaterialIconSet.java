48b8b2f21b019989e0b1d2d984a345cafef96065
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-06-01 01:46:27 +0900

    各種修正

--- src/main/java/gtne/api/unification/material/info/GTNEMaterialIconSet.java
+++ src/main/java/gtne/api/unification/material/info/GTNEMaterialIconSet.java
@@ -2,10 +2,15 @@ package gtne.api.unification.material.info;
 
 import gregtech.api.unification.material.info.MaterialIconSet;
 
+import javax.annotation.Nonnull;
+
 import static gregtech.api.unification.material.info.MaterialIconSet.DULL;
 
-public class GTNEMaterialIconSet {
+public class GTNEMaterialIconSet extends MaterialIconSet{
 
     public static final MaterialIconSet INFINITY = new MaterialIconSet("infinity", DULL, true);
 
+    public GTNEMaterialIconSet(@Nonnull String name) {
+        super(name);
+    }
 }
