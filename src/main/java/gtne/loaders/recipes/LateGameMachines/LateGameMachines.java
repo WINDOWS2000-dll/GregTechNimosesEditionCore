754007b32e15d3d5f5d724063ec66426a3844c41
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-09-04 01:30:54 +0900

    レシピ追加テスト(LuV機械)

--- src/main/java/gtne/loaders/recipes/LateGameMachines/LateGameMachines.java
+++ src/main/java/gtne/loaders/recipes/LateGameMachines/LateGameMachines.java
@@ -2,8 +2,19 @@ package gtne.loaders.recipes.LateGameMachines;
 
 import gregtech.api.GTValues;
 import gregtech.api.recipes.*;
+import gregtech.api.unification.material.MarkerMaterial;
+import gregtech.api.unification.material.MarkerMaterials;
+import gregtech.api.unification.ore.OrePrefix;
+import gregtech.common.blocks.BlockMachineCasing;
+import gregtech.common.blocks.MetaBlocks;
 import gtne.common.ConfigHolder;
 
+import static gregtech.api.unification.ore.OrePrefix.*;
+import static gregtech.common.metatileentities.MetaTileEntities.*;
+import static gregtech.loaders.recipe.MetaTileEntityLoader.registerMachineRecipe;
+import static gregtech.api.GTValues.*;
+import static gregtech.api.unification.material.Materials.*;
+import static gregtech.common.items.MetaItems.*;
 import static gtne.api.util.GTNEUtil.GTNEMachineRecipeName;
 
 public class LateGameMachines {
@@ -30,6 +41,48 @@ public class LateGameMachines {
 
     //LuV Machine Recipe Change
     private static void ChangeLuVMachineRecipe() {
+
+        //Electric Furnace
+        RecipeMaps.ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .inputs(MetaBlocks.MACHINE_CASING.getItemVariant(BlockMachineCasing.MachineCasingType.LuV))
+                .input(circuit, MarkerMaterials.Tier.LuV, 4)
+                .input(wireGtQuadruple, HSSG, 6)
+                .input(VOLTAGE_COIL_LuV, 4)
+                .input(HIGH_POWER_INTEGRATED_CIRCUIT, 2)
+                .input(CARBON_FIBER_PLATE, 4)
+                .input(cableGtSingle, HSSG, 4)
+                .fluidInputs(SolderingAlloy.getFluid(1440))
+                .fluidInputs(TungstenSteel.getFluid(288))
+                .outputs(ELECTRIC_FURNACE[6].getStackForm())
+                .research(b -> b
+                        .researchStack(ELECTRIC_FURNACE[5].getStackForm())
+                        .EUt(4096)
+                        .duration(12000))
+                .EUt(10000).duration(600)
+                .buildAndRegister();
+
+        //Macerator
+        RecipeMaps.ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .inputs(MetaBlocks.MACHINE_CASING.getItemVariant(BlockMachineCasing.MachineCasingType.LuV))
+                .input(circuit, MarkerMaterials.Tier.LuV, 6)
+                .input(ELECTRIC_MOTOR_LuV, 2)
+                .input(gear, TungstenSteel, 2)
+                .input(gearSmall, TungstenSteel, 4)
+                .input(COMPONENT_GRINDER_TUNGSTEN, 2)
+                .input(HIGH_POWER_INTEGRATED_CIRCUIT, 2)
+                .input(CARBON_FIBER_PLATE, 4)
+                .input(cableGtSingle, HSSG, 4)
+                .fluidInputs(SolderingAlloy.getFluid(1440))
+                .fluidInputs(TungstenSteel.getFluid(288))
+                .outputs(MACERATOR[6].getStackForm())
+                .research(b -> b
+                        .researchStack(MACERATOR[5].getStackForm())
+                        .EUt(4096)
+                        .duration(12000))
+                .EUt(10000).duration(600)
+                .buildAndRegister();
+
+
     }
 
     //ZPM Machine Recipe Remove
