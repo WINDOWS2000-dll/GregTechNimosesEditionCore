46f8018b3b7a05400c7eda910c5706670ba4d9de
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-09-04 00:21:31 +0900

    LuV層以降の機械のレシピの削除

--- src/main/java/gtne/loaders/recipes/LateGameMachines/LateGameMachines.java
+++ src/main/java/gtne/loaders/recipes/LateGameMachines/LateGameMachines.java
@@ -11,16 +11,51 @@ public class LateGameMachines {
     public static void init() {
             if (ConfigHolder.recipeoption.Super_HardMode_LateGame) {
                 RemoveLuVMachinesRecipe();
+                ChangeLuVMachineRecipe();
+                RemoveZPMMachineRecipe();
+                ChangeZPMMachineRecipe();
+                RemoveUVMachineRecipe();
+                ChangeUVMachineRecipe();
         }
     }
 
-    //LuV
+    //LuV Machine Recipe Remove
     private static void RemoveLuVMachinesRecipe() {
 
         for (int i = 0; i < GTNEMachineRecipeName.length; i++) {
             ModHandler.removeRecipeByName("gregtech:gregtech.machine." + GTNEMachineRecipeName[i] + ".luv");
         }
 
+    }
+
+    //LuV Machine Recipe Change
+    private static void ChangeLuVMachineRecipe() {
+    }
+
+    //ZPM Machine Recipe Remove
+    private static void RemoveZPMMachineRecipe() {
+
+        for (int i = 0; i < GTNEMachineRecipeName.length; i++) {
+            ModHandler.removeRecipeByName("gregtech:gregtech.machine." + GTNEMachineRecipeName[i] + ".zpm");
+        }
+
+    }
+
+    //ZPM Machine Recipe Change
+    private static void ChangeZPMMachineRecipe() {
+    }
+
+    //UV Machine Recipe Remove
+    private static void RemoveUVMachineRecipe() {
+
+        for (int i = 0; i < GTNEMachineRecipeName.length; i++) {
+            ModHandler.removeRecipeByName("gregtech:gregtech.machine." + GTNEMachineRecipeName[i] + ".uv");
+        }
+
+    }
+
+    //UV Machine Recipe Change
+    private static void ChangeUVMachineRecipe() {
     }
 
 }
