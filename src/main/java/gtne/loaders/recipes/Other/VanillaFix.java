3cac25beb26266c25f0f921f6bcd4b90b912dafb
WINDOWS2000-dll <windows2000.exe@gmail.com>
2023-12-25 19:09:19 +0900

    各種バニラレシピの調整

--- src/main/java/gtne/loaders/recipes/Other/VanillaFix.java
+++ src/main/java/gtne/loaders/recipes/Other/VanillaFix.java
@@ -63,13 +63,33 @@ public class VanillaFix {
             GTRecipeHandler.removeRecipesByInputs(RecipeMaps.ALLOY_SMELTER_RECIPES, new ItemStack(Items.CLAY_BALL, 1), SHAPE_MOLD_INGOT.getStackForm(1));
             //Compressed Clay
             ModHandler.removeRecipeByName("gregtech:compressed_clay");
+        //WoodenTools
+            ModHandler.removeRecipeByName("minecraft:wooden_pickaxe");
+            ModHandler.removeRecipeByName("minecraft:wooden_sword");
+            ModHandler.removeRecipeByName("minecraft:wooden_shovel");
+            ModHandler.removeRecipeByName("minecraft:wooden_axe");
+            ModHandler.removeRecipeByName("minecraft:wooden_hoe");
+        //Armor Stand
+            ModHandler.removeRecipeByName("minecraft:armor_stand");
+        //Vanilla Armor
+            ModHandler.removeRecipeByName("minecraft:leather_helmet");
+            ModHandler.removeRecipeByName("minecraft:leather_chestplate");
+            ModHandler.removeRecipeByName("minecraft:leather_leggings");
+            ModHandler.removeRecipeByName("minecraft:leather_boots");
+            ModHandler.removeRecipeByName("gregtech:iron_helmet");
+            ModHandler.removeRecipeByName("gregtech:iron_chestplate");
+            ModHandler.removeRecipeByName("gregtech:iron_leggings");
+            ModHandler.removeRecipeByName("gregtech:iron_boots");
+            ModHandler.removeRecipeByName("gregtech:golden_helmet");
+            ModHandler.removeRecipeByName("gregtech:golden_chestplate");
+            ModHandler.removeRecipeByName("gregtech:golden_leggings");
+            ModHandler.removeRecipeByName("gregtech:golden_boots");
+        //Hopper
+            ModHandler.removeRecipeByName("gregtech:hopper");
     }
 
     private static void RemoveAddonRecipes() {
-        ModHandler.removeRecipeByName("enderio:tweak_wood_hopper");
-        ModHandler.removeRecipeByName("enderio:tweak_stick_from_wood");
         ModHandler.removeRecipeByName("gregtech:stick_normal");
-        ModHandler.removeRecipeByName("enderio:tweak_chest_from_wood");
         ModHandler.removeRecipeByName("enderio:compatibility_glass_pane");
         ModHandler.removeRecipeByName("thermalfoundation:saddle");
 
@@ -94,7 +114,7 @@ public class VanillaFix {
                 'F', new ItemStack(Items.FLINT)
         );
         //Bed GTNE
-        ModHandler.addShapedRecipe("bed", new ItemStack(Blocks.BED, 1, 0),
+        ModHandler.addShapedRecipe("bed", new ItemStack(Items.BED, 1, 0),
                 "WWW",
                         "SSS",
                         "FIF",
@@ -105,12 +125,12 @@ public class VanillaFix {
         );
         //Bed Stain
         for (int i = 0; i < 16; i++) {
-            ModHandler.addShapedRecipe("bed_colored_" + i, new ItemStack(Blocks.BED, 1, i),
+            ModHandler.addShapedRecipe("bed_colored_" + i, new ItemStack(Items.BED, 1, i),
                             "DDD",
                                     "DBD",
                                     "DDD",
                     'D', new ItemStack(Items.DYE, 1, i),
-                    'B', new ItemStack(Blocks.BED, 1, 0)
+                    'B', new ItemStack(Items.BED, 1, 0)
             );
         }
         //Fence
@@ -245,9 +265,8 @@ public class VanillaFix {
 
             //Compressed Clay
             ModHandler.addShapelessRecipe("compressed_clay", new ItemStack(COMPRESSED_CLAY.getStackForm().getItem()),
-                    "wc",
-                    'w', new ItemStack(BRICK_COMPOUND.getStackForm().getItem()),
-                    'c', new ItemStack(WOODEN_FORM_BRICK.getStackForm().getItem())
+                new ItemStack(BRICK_COMPOUND.getStackForm().getItem()),
+                new ItemStack(WOODEN_FORM_BRICK.getStackForm().getItem())
             );
 
             //Alloy Smelter Brick Recipe
@@ -257,5 +276,229 @@ public class VanillaFix {
                     .output(Items.BRICK, 1)
                     .EUt(2).duration(200)
                     .buildAndRegister();
+        //Wooden Half Block Recipe
+            //Oak
+            ModHandler.addShapedRecipe("oak_half_block", new ItemStack(Blocks.WOODEN_SLAB, 2, 0),
+                    "  s",
+                            " W ",
+                            "   ",
+                    'W', new ItemStack(Blocks.PLANKS, 1, 0)
+            );
+            //Spruce
+            ModHandler.addShapedRecipe("spruce_half_block", new ItemStack(Blocks.WOODEN_SLAB, 2, 1),
+                    "  s",
+                            " W ",
+                            "   ",
+                    'W', new ItemStack(Blocks.PLANKS, 1, 1)
+            );
+            //Birch
+            ModHandler.addShapedRecipe("birch_half_block", new ItemStack(Blocks.WOODEN_SLAB, 2, 2),
+                    "  s",
+                            " W ",
+                            "   ",
+                    'W', new ItemStack(Blocks.PLANKS, 1, 2)
+            );
+            //Jungle
+            ModHandler.addShapedRecipe("jungle_half_block", new ItemStack(Blocks.WOODEN_SLAB, 2, 3),
+                    "  s",
+                            " W ",
+                            "   ",
+                    'W', new ItemStack(Blocks.PLANKS, 1, 3)
+            );
+            //Acacia
+            ModHandler.addShapedRecipe("acacia_half_block", new ItemStack(Blocks.WOODEN_SLAB, 2, 4),
+                    "  s",
+                            " W ",
+                            "   ",
+                    'W', new ItemStack(Blocks.PLANKS, 1, 4)
+            );
+            //Dark Oak
+            ModHandler.addShapedRecipe("dark_oak_half_block", new ItemStack(Blocks.WOODEN_SLAB, 2, 5),
+                    "  s",
+                            " W ",
+                            "   ",
+                    'W', new ItemStack(Blocks.PLANKS, 1, 5));
+        //WoodenTools
+            //Pickaxe
+            ModHandler.addShapedRecipe("wooden_tool_pickaxe", new ItemStack(Items.WOODEN_PICKAXE, 1),
+                    "WWW",
+                            "CSC",
+                            "FSF",
+                    'W', OreDictUnifier.get(plank, Wood, 1),
+                    'C', OreDictUnifier.get(screw, Wood, 1),
+                    'S', new ItemStack(Items.STICK),
+                    'F', new ItemStack(Items.FLINT)
+                    );
+            //Sword
+            ModHandler.addShapedRecipe("wooden_tool_sword", new ItemStack(Items.WOODEN_SWORD, 1),
+                    "CWF",
+                            "FWF",
+                            "FSC",
+                    'W', OreDictUnifier.get(plank, Wood, 1),
+                    'C', OreDictUnifier.get(screw, Wood, 1),
+                    'S', new ItemStack(Items.STICK),
+                    'F', new ItemStack(Items.FLINT)
+            );
+            //Shovel
+            ModHandler.addShapedRecipe("wooden_tool_shovel", new ItemStack(Items.WOODEN_SHOVEL, 1),
+                    "FWF",
+                            "CSC",
+                            " S ",
+                    'W', OreDictUnifier.get(plank, Wood, 1),
+                    'C', OreDictUnifier.get(screw, Wood, 1),
+                    'S', new ItemStack(Items.STICK),
+                    'F', new ItemStack(Items.FLINT)
+            );
+            //Axe
+            ModHandler.addShapedRecipe("wooden_tool_axe", new ItemStack(Items.WOODEN_AXE, 1),
+                    "FWF",
+                            "SFW",
+                            "CSF",
+                    'W', OreDictUnifier.get(plank, Wood, 1),
+                    'C', OreDictUnifier.get(screw, Wood, 1),
+                    'S', new ItemStack(Items.STICK),
+                    'F', new ItemStack(Items.FLINT)
+            );
+            //Hoe
+            ModHandler.addShapedRecipe("wooden_tool_hoe", new ItemStack(Items.WOODEN_HOE, 1),
+                    "CWW",
+                            " SF",
+                            " SC",
+                    'W', OreDictUnifier.get(plank, Wood, 1),
+                    'C', OreDictUnifier.get(screw, Wood, 1),
+                    'S', new ItemStack(Items.STICK),
+                    'F', new ItemStack(Items.FLINT)
+            );
+        //Super Nerf Stick Recipe
+            ModHandler.addShapedRecipe("nerfed_stick_recipe", new ItemStack(Items.STICK),
+                    "W",
+                            "W",
+                    'W', new UnificationEntry(plank, Wood)
+            );
+        //Armor Stand
+            ModHandler.addShapedRecipe("armor_stand", new ItemStack(Items.ARMOR_STAND, 1),
+                    "SSS",
+                            "CSd",
+                            "SHS",
+                    'S', OreDictUnifier.get(stickLong, Wood, 1),
+                    'C', OreDictUnifier.get(screw, Iron, 1),
+                    'H', new ItemStack(Blocks.STONE_SLAB, 1, 0)
+            );
+        //Leather Armor
+            //helmet
+                ModHandler.addShapedRecipe("leather_helmet", new ItemStack(Items.LEATHER_HELMET, 1),
+                        "LLL",
+                                "LkL",
+                                "   ",
+                        'L', new ItemStack(Items.LEATHER)
+                );
+            //Chest Plate
+                ModHandler.addShapedRecipe("leather_chest_plate", new ItemStack(Items.LEATHER_CHESTPLATE, 1),
+                        "LkL",
+                                "LLL",
+                                "LLL",
+                        'L', new ItemStack(Items.LEATHER)
+                );
+            //Leggings
+                ModHandler.addShapedRecipe("leather_leggings", new ItemStack(Items.LEATHER_LEGGINGS),
+                        "LLL",
+                                "LkL",
+                                "L L",
+                        'L', new ItemStack(Items.LEATHER)
+                );
+            //Boots
+                ModHandler.addShapedRecipe("leather_boot", new ItemStack(Items.LEATHER_BOOTS, 1),
+                        "   ",
+                                "L L",
+                                "LkL",
+                        'L', new ItemStack(Items.LEATHER)
+                );
+        //Iron Armor
+            //helmet
+                ModHandler.addShapedRecipe("iron_helmet", new ItemStack(Items.IRON_HELMET),
+                        "DDD",
+                                "PhP",
+                                "SdS",
+                        'D', OreDictUnifier.get(plateDouble, Iron, 1),
+                        'P', OreDictUnifier.get(plate,Iron, 1),
+                        'S', OreDictUnifier.get(screw, Iron, 1)
+                );
+            //chestplate
+                ModHandler.addShapedRecipe("iron_chestplate", new ItemStack(Items.IRON_CHESTPLATE, 1),
+                        "DhD",
+                                "PDP",
+                                "DPD",
+                        'D', OreDictUnifier.get(plateDouble, Iron, 1),
+                        'P', OreDictUnifier.get(plate,Iron, 1)
+                );
+            //Leggings
+                ModHandler.addShapedRecipe("iron_leggings", new ItemStack(Items.IRON_LEGGINGS, 1),
+                        "DDD",
+                                "PhP",
+                                "P P",
+                        'D', OreDictUnifier.get(plateDouble, Iron, 1),
+                        'P', OreDictUnifier.get(plate,Iron, 1)
+                );
+            //Boots
+                ModHandler.addShapedRecipe("iron_boots", new ItemStack(Items.IRON_BOOTS, 1),
+                        "S S",
+                                "DdD",
+                                "DhD",
+                        'D', OreDictUnifier.get(plateDouble, Iron, 1),
+                        'P', OreDictUnifier.get(plate,Iron, 1),
+                        'S', OreDictUnifier.get(screw, Iron, 1)
+                );
+        //THE GOLDEN AGE WILL RETURN AGAIN Armor
+            //helmet
+                ModHandler.addShapedRecipe("golden_helmet", new ItemStack(Items.GOLDEN_HELMET),
+                        "DDD",
+                                "PhP",
+                                "SdS",
+                        'D', OreDictUnifier.get(plateDouble, Gold, 1),
+                        'P', OreDictUnifier.get(plate, Gold, 1),
+                        'S', OreDictUnifier.get(screw, Gold, 1)
+                );
+            //chestplate
+                ModHandler.addShapedRecipe("golden_chestplate", new ItemStack(Items.GOLDEN_CHESTPLATE, 1),
+                        "DhD",
+                                "PDP",
+                                "DPD",
+                        'D', OreDictUnifier.get(plateDouble, Gold, 1),
+                        'P', OreDictUnifier.get(plate, Gold, 1)
+                );
+            //Leggings
+                ModHandler.addShapedRecipe("golden_leggings", new ItemStack(Items.GOLDEN_LEGGINGS, 1),
+                        "DDD",
+                                "PhP",
+                                "P P",
+                        'D', OreDictUnifier.get(plateDouble, Gold, 1),
+                        'P', OreDictUnifier.get(plate, Gold, 1)
+                );
+            //Boots
+                ModHandler.addShapedRecipe("golden_boots", new ItemStack(Items.GOLDEN_BOOTS, 1),
+                        "S S",
+                                "DdD",
+                                "DhD",
+                        'D', OreDictUnifier.get(plateDouble, Gold, 1),
+                        'P', OreDictUnifier.get(plate, Gold, 1),
+                        'S', OreDictUnifier.get(screw, Gold, 1)
+                );
+        //Wooden Screw Recipe
+            ModHandler.addShapedRecipe("easy_wooden_screw", new ItemStack(OreDictUnifier.get(screw, Wood).getItem(), 2),
+                    "S F",
+                    'S', new ItemStack(Items.STICK),
+                    'F', new ItemStack(Items.FLINT)
+            );
+        //Hopper
+            ModHandler.addShapedRecipe("hopper", new ItemStack(Blocks.HOPPER, 1),
+                    "DSD",
+                            "GCG",
+                            "hDw",
+                    'D', OreDictUnifier.get(plateDouble, Iron, 1),
+                    'S', OreDictUnifier.get(spring, Iron, 1),
+                    'G', OreDictUnifier.get(gear, Iron, 1),
+                    'C', new ItemStack(Blocks.CHEST)
+            );
+
     }
 }
